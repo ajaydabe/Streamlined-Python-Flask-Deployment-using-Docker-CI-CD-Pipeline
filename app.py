b1d1fd0b764e06f8d1ed4ecdcf08746b2f179736
Ajay Dabe <160045230+ajaydabe@users.noreply.github.com>
2024-06-24 15:31:16 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, this is a simple web application!'
+    return 'Congratulation !!! Our Application is working !'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
